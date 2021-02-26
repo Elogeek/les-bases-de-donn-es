eff12d8c49b8cccc2d9f957c049724b504337720
Elogeek <elodiechristin@gmail.com>
2021-02-26 11:35:14 +0100

    g

--- config-phpstorm.php
+++ config-phpstorm.php
@@ -3,7 +3,7 @@
 require './versionStatic.php';
 $db = DB2::getInstance();
 
-$requeteSQL = "INSERT INTO démo relation.user(id, nom, prenom, password) VALUES ()";
+$requeteSQL = "INSERT INTO demo_relation.user(id, nom, prenom, password) VALUES ()";
 //cliquer database, cliquer +, cliquer sur datasource puis mysql puis advanced puis rechercher servertimezone  mot de passe: UTC
 
 $db->exec($requeteSQL);
